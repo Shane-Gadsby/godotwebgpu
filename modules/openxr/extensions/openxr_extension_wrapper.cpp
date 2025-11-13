godotengine/godot

OpenXR: Add support for frame synthesis

modules/openxr/extensions/openxr_extension_wrapper.cpp
@@ -45,6 +45,9 @@ void OpenXRExtensionWrapper::_bind_methods() {
 	GDVIRTUAL_BIND(_set_frame_end_info_and_get_next_pointer, "next_pointer");
 	GDVIRTUAL_BIND(_set_view_locate_info_and_get_next_pointer, "next_pointer");
 	GDVIRTUAL_BIND(_set_reference_space_create_info_and_get_next_pointer, "reference_space_type", "next_pointer");
+	GDVIRTUAL_BIND(_set_view_configuration_view_count, "view_count");
+	GDVIRTUAL_BIND(_set_view_configuration_and_get_next_pointer, "view", "next_pointer");
+	GDVIRTUAL_BIND(_print_view_configuration_info, "view");
 	GDVIRTUAL_BIND(_get_composition_layer_count);
 	GDVIRTUAL_BIND(_get_composition_layer, "index");
 	GDVIRTUAL_BIND(_get_composition_layer_order, "index");
@@ -183,6 +186,24 @@ void *OpenXRExtensionWrapper::set_frame_end_info_and_get_next_pointer(void *p_ne
 	}
 
 	return nullptr;
+}
+
+void OpenXRExtensionWrapper::set_view_configuration_view_count(uint32_t p_view_count) {
+	GDVIRTUAL_CALL(_set_view_configuration_view_count, p_view_count);
+}
+
+void *OpenXRExtensionWrapper::set_view_configuration_and_get_next_pointer(uint32_t p_view, void *p_next_pointer) {
+	uint64_t pointer = 0;
+
+	if (GDVIRTUAL_CALL(_set_view_configuration_and_get_next_pointer, p_view, GDExtensionPtr<void>(p_next_pointer), pointer)) {
+		return reinterpret_cast<void *>(pointer);
+	}
+
+	return nullptr;
+}
+
+void OpenXRExtensionWrapper::print_view_configuration_info(uint32_t p_view) const {
+	GDVIRTUAL_CALL(_print_view_configuration_info, p_view);
 }
 
 void *OpenXRExtensionWrapper::set_view_locate_info_and_get_next_pointer(void *p_next_pointer) {

modules/openxr/extensions/openxr_extension_wrapper.h
@@ -83,6 +83,10 @@ public:
 	virtual void *set_view_locate_info_and_get_next_pointer(void *p_next_pointer); // Add additional data structures when calling xrLocateViews
 	virtual void *set_frame_end_info_and_get_next_pointer(void *p_next_pointer); // Add additional data structures when calling xrEndFrame
 
+	virtual void set_view_configuration_view_count(uint32_t p_view_count);
+	virtual void *set_view_configuration_and_get_next_pointer(uint32_t p_view, void *p_next_pointer); // Add additional data structures when calling xrEnumerateViewConfiguration
+	virtual void print_view_configuration_info(uint32_t p_view) const;
+
 	//TODO workaround as GDExtensionPtr<void> return type results in build error in godot-cpp
 	GDVIRTUAL1R(uint64_t, _set_system_properties_and_get_next_pointer, GDExtensionPtr<void>);
 	GDVIRTUAL1R(uint64_t, _set_instance_create_info_and_get_next_pointer, GDExtensionPtr<void>);
@@ -97,6 +101,9 @@ public:
 	GDVIRTUAL0R(int, _get_composition_layer_count);
 	GDVIRTUAL1R(uint64_t, _get_composition_layer, int);
 	GDVIRTUAL1R(int, _get_composition_layer_order, int);
+	GDVIRTUAL1(_set_view_configuration_view_count, int);
+	GDVIRTUAL2R(uint64_t, _set_view_configuration_and_get_next_pointer, uint32_t, GDExtensionPtr<void>);
+	GDVIRTUAL1C(_print_view_configuration_info, int);
 
 	virtual PackedStringArray get_suggested_tracker_names();
 

modules/openxr/extensions/openxr_frame_synthesis_extension.h
@@ -1,0 +1,106 @@
+/**************************************************************************/
+/*  openxr_frame_synthesis_extension.h                                    */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "../openxr_api.h"
+#include "openxr_extension_wrapper.h"
+
+#include <openxr/openxr.h>
+
+class OpenXRFrameSynthesisExtension : public OpenXRExtensionWrapper {
+	GDCLASS(OpenXRFrameSynthesisExtension, OpenXRExtensionWrapper);
+
+public:
+	static OpenXRFrameSynthesisExtension *get_singleton();
+
+	OpenXRFrameSynthesisExtension();
+	virtual ~OpenXRFrameSynthesisExtension() override;
+
+	virtual HashMap<String, bool *> get_requested_extensions() override;
+
+	virtual void on_instance_created(const XrInstance p_instance) override;
+	virtual void on_instance_destroyed() override;
+
+	virtual void set_view_configuration_view_count(uint32_t p_view_count) override;
+	virtual void *set_view_configuration_and_get_next_pointer(uint32_t p_view, void *p_next_pointer) override;
+	virtual void print_view_configuration_info(uint32_t p_view) const override;
+
+	virtual void on_session_created(const XrSession p_session) override;
+	virtual void on_session_destroyed() override;
+
+	virtual void on_main_swapchains_created() override;
+	virtual void on_pre_render() override;
+	virtual void on_post_draw_viewport(RID p_render_target) override;
+	virtual void *set_projection_views_and_get_next_pointer(int p_view_index, void *p_next_pointer) override;
+
+	bool is_available() const;
+
+	bool is_enabled() const;
+	void set_enabled(bool p_enabled);
+
+	bool get_relax_frame_interval() const;
+	void set_relax_frame_interval(bool p_relax_frame_interval);
+
+	void skip_next_frame();
+
+protected:
+	static void _bind_methods();
+
+	void _set_render_state_enabled(bool p_enabled);
+	void _set_rt_relax_frame_interval(bool p_relax_frame_interval);
+	void _set_rt_skip_next_frame();
+
+private:
+	enum SwapChainTypes {
+		SWAPCHAIN_MOTION_VECTOR,
+		SWAPCHAIN_DEPTH,
+		SWAPCHAIN_MAX
+	};
+	void free_swapchains();
+
+	static OpenXRFrameSynthesisExtension *singleton;
+
+	bool frame_synthesis_ext = false;
+	bool enabled = true;
+	bool relax_frame_interval = false;
+
+	// Frame synthesis render state, only accessible on render thread
+	struct RenderState {
+		bool enabled = true;
+		bool relax_frame_interval = false;
+		bool skip_next_frame = false;
+
+		LocalVector<XrFrameSynthesisConfigViewEXT> config_views;
+		OpenXRAPI::OpenXRSwapChainInfo swapchains[SWAPCHAIN_MAX];
+		LocalVector<XrFrameSynthesisInfoEXT> frame_synthesis_info;
+		Transform3D previous_transform;
+	} render_state;
+};

modules/openxr/openxr_api.cpp
@@ -815,23 +815,45 @@ bool OpenXRAPI::load_supported_view_configuration_views(XrViewConfigurationType 
 	}
 
 	view_configuration_views.resize(view_count);
+	for (OpenXRExtensionWrapper *extension : registered_extension_wrappers) {
+		extension->set_view_configuration_view_count(view_count);
+	}
 
+	uint32_t view = 0;
 	for (XrViewConfigurationView &view_configuration_view : view_configuration_views) {
 		view_configuration_view.type = XR_TYPE_VIEW_CONFIGURATION_VIEW;
 		view_configuration_view.next = nullptr;
+
+		for (OpenXRExtensionWrapper *extension : registered_extension_wrappers) {
+			void *np = extension->set_view_configuration_and_get_next_pointer(view, view_configuration_view.next);
+			if (np != nullptr) {
+				view_configuration_view.next = np;
+			}
+		}
+
+		view++;
 	}
 
 	result = xrEnumerateViewConfigurationViews(instance, system_id, p_configuration_type, view_count, &view_count, view_configuration_views.ptr());
 	ERR_FAIL_COND_V_MSG(XR_FAILED(result), false, "OpenXR: Failed to enumerate view configurations");
 
-	for (const XrViewConfigurationView &view_configuration_view : view_configuration_views) {
-		print_verbose("OpenXR: Found supported view configuration view");
-		print_verbose(String(" - width: ") + itos(view_configuration_view.maxImageRectWidth));
-		print_verbose(String(" - height: ") + itos(view_configuration_view.maxImageRectHeight));
-		print_verbose(String(" - sample count: ") + itos(view_configuration_view.maxSwapchainSampleCount));
-		print_verbose(String(" - recommended render width: ") + itos(view_configuration_view.recommendedImageRectWidth));
-		print_verbose(String(" - recommended render height: ") + itos(view_configuration_view.recommendedImageRectHeight));
-		print_verbose(String(" - recommended render sample count: ") + itos(view_configuration_view.recommendedSwapchainSampleCount));
+	if (is_print_verbose_enabled()) {
+		view = 0;
+		for (const XrViewConfigurationView &view_configuration_view : view_configuration_views) {
+			print_line("OpenXR: Found supported view configuration view");
+			print_line(" - width: ", itos(view_configuration_view.maxImageRectWidth));
+			print_line(" - height: ", itos(view_configuration_view.maxImageRectHeight));
+			print_line(" - sample count: ", itos(view_configuration_view.maxSwapchainSampleCount));
+			print_line(" - recommended render width: ", itos(view_configuration_view.recommendedImageRectWidth));
+			print_line(" - recommended render height: ", itos(view_configuration_view.recommendedImageRectHeight));
+			print_line(" - recommended render sample count: ", itos(view_configuration_view.recommendedSwapchainSampleCount));
+
+			for (OpenXRExtensionWrapper *extension : registered_extension_wrappers) {
+				extension->print_view_configuration_info(view);
+			}
+
+			view++;
+		}
 	}
 
 	return true;
@@ -1371,6 +1393,9 @@ void OpenXRAPI::destroy_session() {
 		for (OpenXRExtensionWrapper *wrapper : registered_extension_wrappers) {
 			wrapper->on_session_destroyed();
 		}
+
+		// Rerun this just in case any of our extensions freed up swapchains.
+		OpenXRSwapChainInfo::free_queued();
 
 		end_debug_label_region();
 

Shane-Gadsby/godotwebgpu

modules/openxr/extensions/openxr_extension_wrapper.cpp
@@ -45,6 +45,9 @@ void OpenXRExtensionWrapper::_bind_methods() {
 	GDVIRTUAL_BIND(_set_frame_end_info_and_get_next_pointer, "next_pointer");
 	GDVIRTUAL_BIND(_set_view_locate_info_and_get_next_pointer, "next_pointer");
 	GDVIRTUAL_BIND(_set_reference_space_create_info_and_get_next_pointer, "reference_space_type", "next_pointer");
+	GDVIRTUAL_BIND(_prepare_view_configuration, "view_count");
+	GDVIRTUAL_BIND(_set_view_configuration_and_get_next_pointer, "view", "next_pointer");
+	GDVIRTUAL_BIND(_print_view_configuration_info, "view");
 	GDVIRTUAL_BIND(_get_composition_layer_count);
 	GDVIRTUAL_BIND(_get_composition_layer, "index");
 	GDVIRTUAL_BIND(_get_composition_layer_order, "index");
@@ -183,6 +186,24 @@ void *OpenXRExtensionWrapper::set_frame_end_info_and_get_next_pointer(void *p_ne
 	}
 
 	return nullptr;
+}
+
+void OpenXRExtensionWrapper::prepare_view_configuration(uint32_t p_view_count) {
+	GDVIRTUAL_CALL(_prepare_view_configuration, p_view_count);
+}
+
+void *OpenXRExtensionWrapper::set_view_configuration_and_get_next_pointer(uint32_t p_view, void *p_next_pointer) {
+	uint64_t pointer = 0;
+
+	if (GDVIRTUAL_CALL(_set_view_configuration_and_get_next_pointer, p_view, GDExtensionPtr<void>(p_next_pointer), pointer)) {
+		return reinterpret_cast<void *>(pointer);
+	}
+
+	return nullptr;
+}
+
+void OpenXRExtensionWrapper::print_view_configuration_info(uint32_t p_view) const {
+	GDVIRTUAL_CALL(_print_view_configuration_info, p_view);
 }
 
 void *OpenXRExtensionWrapper::set_view_locate_info_and_get_next_pointer(void *p_next_pointer) {

modules/openxr/extensions/openxr_extension_wrapper.h
@@ -85,6 +85,10 @@ public:
 	virtual void *set_view_locate_info_and_get_next_pointer(void *p_next_pointer); // Add additional data structures when calling xrLocateViews
 	virtual void *set_frame_end_info_and_get_next_pointer(void *p_next_pointer); // Add additional data structures when calling xrEndFrame
 
+	virtual void prepare_view_configuration(uint32_t p_view_count);
+	virtual void *set_view_configuration_and_get_next_pointer(uint32_t p_view, void *p_next_pointer); // Add additional data structures when calling xrEnumerateViewConfiguration
+	virtual void print_view_configuration_info(uint32_t p_view) const;
+
 	//TODO workaround as GDExtensionPtr<void> return type results in build error in godot-cpp
 	GDVIRTUAL1R(uint64_t, _set_system_properties_and_get_next_pointer, GDExtensionPtr<void>);
 	GDVIRTUAL1R(uint64_t, _set_instance_create_info_and_get_next_pointer, GDExtensionPtr<void>);
@@ -99,6 +103,9 @@ public:
 	GDVIRTUAL0R(int, _get_composition_layer_count);
 	GDVIRTUAL1R(uint64_t, _get_composition_layer, int);
 	GDVIRTUAL1R(int, _get_composition_layer_order, int);
+	GDVIRTUAL1(_prepare_view_configuration, int);
+	GDVIRTUAL2R(uint64_t, _set_view_configuration_and_get_next_pointer, uint32_t, GDExtensionPtr<void>);
+	GDVIRTUAL1C(_print_view_configuration_info, int);
 
 	virtual PackedStringArray get_suggested_tracker_names();
 

modules/openxr/extensions/openxr_frame_synthesis_extension.h
@@ -1,0 +1,105 @@
+/**************************************************************************/
+/*  openxr_frame_synthesis_extension.h                                    */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "../openxr_api.h"
+#include "openxr_extension_wrapper.h"
+
+#include <openxr/openxr.h>
+
+class OpenXRFrameSynthesisExtension : public OpenXRExtensionWrapper {
+	GDCLASS(OpenXRFrameSynthesisExtension, OpenXRExtensionWrapper);
+
+public:
+	static OpenXRFrameSynthesisExtension *get_singleton();
+
+	OpenXRFrameSynthesisExtension();
+	virtual ~OpenXRFrameSynthesisExtension() override;
+
+	virtual HashMap<String, bool *> get_requested_extensions() override;
+
+	virtual void on_instance_created(const XrInstance p_instance) override;
+	virtual void on_instance_destroyed() override;
+
+	virtual void prepare_view_configuration(uint32_t p_view_count) override;
+	virtual void *set_view_configuration_and_get_next_pointer(uint32_t p_view, void *p_next_pointer) override;
+	virtual void print_view_configuration_info(uint32_t p_view) const override;
+
+	virtual void on_session_destroyed() override;
+
+	virtual void on_main_swapchains_created() override;
+	virtual void on_pre_render() override;
+	virtual void on_post_draw_viewport(RID p_render_target) override;
+	virtual void *set_projection_views_and_get_next_pointer(int p_view_index, void *p_next_pointer) override;
+
+	bool is_available() const;
+
+	bool is_enabled() const;
+	void set_enabled(bool p_enabled);
+
+	bool get_relax_frame_interval() const;
+	void set_relax_frame_interval(bool p_relax_frame_interval);
+
+	void skip_next_frame();
+
+protected:
+	static void _bind_methods();
+
+	void _set_render_state_enabled_rt(bool p_enabled);
+	void _set_relax_frame_interval_rt(bool p_relax_frame_interval);
+	void _set_skip_next_frame_rt();
+
+private:
+	enum SwapchainTypes {
+		SWAPCHAIN_MOTION_VECTOR,
+		SWAPCHAIN_DEPTH,
+		SWAPCHAIN_MAX
+	};
+	void free_swapchains();
+
+	static OpenXRFrameSynthesisExtension *singleton;
+
+	bool frame_synthesis_ext = false;
+	bool enabled = true;
+	bool relax_frame_interval = false;
+
+	// Frame synthesis render state, only accessible on render thread
+	struct RenderState {
+		bool enabled = true;
+		bool relax_frame_interval = false;
+		bool skip_next_frame = false;
+
+		LocalVector<XrFrameSynthesisConfigViewEXT> config_views;
+		OpenXRAPI::OpenXRSwapChainInfo swapchains[SWAPCHAIN_MAX];
+		LocalVector<XrFrameSynthesisInfoEXT> frame_synthesis_info;
+		Transform3D previous_transform;
+	} render_state;
+};

modules/openxr/openxr_api.cpp
@@ -818,23 +818,45 @@ bool OpenXRAPI::load_supported_view_configuration_views(XrViewConfigurationType 
 	}
 
 	view_configuration_views.resize(view_count);
+	for (OpenXRExtensionWrapper *extension : registered_extension_wrappers) {
+		extension->prepare_view_configuration(view_count);
+	}
 
+	uint32_t view = 0;
 	for (XrViewConfigurationView &view_configuration_view : view_configuration_views) {
 		view_configuration_view.type = XR_TYPE_VIEW_CONFIGURATION_VIEW;
 		view_configuration_view.next = nullptr;
+
+		for (OpenXRExtensionWrapper *extension : registered_extension_wrappers) {
+			void *np = extension->set_view_configuration_and_get_next_pointer(view, view_configuration_view.next);
+			if (np != nullptr) {
+				view_configuration_view.next = np;
+			}
+		}
+
+		view++;
 	}
 
 	result = xrEnumerateViewConfigurationViews(instance, system_id, p_configuration_type, view_count, &view_count, view_configuration_views.ptr());
 	ERR_FAIL_COND_V_MSG(XR_FAILED(result), false, "OpenXR: Failed to enumerate view configurations");
 
-	for (const XrViewConfigurationView &view_configuration_view : view_configuration_views) {
-		print_verbose("OpenXR: Found supported view configuration view");
-		print_verbose(String(" - width: ") + itos(view_configuration_view.maxImageRectWidth));
-		print_verbose(String(" - height: ") + itos(view_configuration_view.maxImageRectHeight));
-		print_verbose(String(" - sample count: ") + itos(view_configuration_view.maxSwapchainSampleCount));
-		print_verbose(String(" - recommended render width: ") + itos(view_configuration_view.recommendedImageRectWidth));
-		print_verbose(String(" - recommended render height: ") + itos(view_configuration_view.recommendedImageRectHeight));
-		print_verbose(String(" - recommended render sample count: ") + itos(view_configuration_view.recommendedSwapchainSampleCount));
+	if (is_print_verbose_enabled()) {
+		view = 0;
+		for (const XrViewConfigurationView &view_configuration_view : view_configuration_views) {
+			print_line("OpenXR: Found supported view configuration view");
+			print_line(" - width: ", itos(view_configuration_view.maxImageRectWidth));
+			print_line(" - height: ", itos(view_configuration_view.maxImageRectHeight));
+			print_line(" - sample count: ", itos(view_configuration_view.maxSwapchainSampleCount));
+			print_line(" - recommended render width: ", itos(view_configuration_view.recommendedImageRectWidth));
+			print_line(" - recommended render height: ", itos(view_configuration_view.recommendedImageRectHeight));
+			print_line(" - recommended render sample count: ", itos(view_configuration_view.recommendedSwapchainSampleCount));
+
+			for (OpenXRExtensionWrapper *extension : registered_extension_wrappers) {
+				extension->print_view_configuration_info(view);
+			}
+
+			view++;
+		}
 	}
 
 	return true;
@@ -1367,6 +1389,9 @@ void OpenXRAPI::destroy_session() {
 		for (OpenXRExtensionWrapper *wrapper : registered_extension_wrappers) {
 			wrapper->on_session_destroyed();
 		}
+
+		// Rerun this just in case any of our extensions freed up swapchains.
+		OpenXRSwapChainInfo::free_queued();
 
 		end_debug_label_region();
 