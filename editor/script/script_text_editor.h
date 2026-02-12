godotengine/godot

Add drag and drop info label

editor/script/script_text_editor.h
@@ -60,6 +60,7 @@ class ScriptTextEditor : public ScriptEditorBase {
 	CodeTextEditor *code_editor = nullptr;
 	RichTextLabel *warnings_panel = nullptr;
 	RichTextLabel *errors_panel = nullptr;
+	Label *drag_info_label = nullptr;
 
 	Ref<Script> script;
 	bool script_is_valid = false;
@@ -214,6 +215,7 @@ protected:
 	void _show_warnings_panel(bool p_show);
 	void _error_clicked(const Variant &p_line);
 	void _warning_clicked(const Variant &p_line);
+	void _on_mouse_exited();
 
 	bool _is_valid_color_info(const Dictionary &p_info);
 	Array _inline_object_parse(const String &p_text);
@@ -248,6 +250,7 @@ protected:
 	Variant get_drag_data_fw(const Point2 &p_point, Control *p_from);
 	bool can_drop_data_fw(const Point2 &p_point, const Variant &p_data, Control *p_from) const;
 	void drop_data_fw(const Point2 &p_point, const Variant &p_data, Control *p_from);
+	void set_drop_info_text(const Dictionary &p_info) const;
 
 	String _get_absolute_path(const String &rel_path);
 

Shane-Gadsby/godotwebgpu

editor/script/script_text_editor.h
@@ -62,6 +62,7 @@ class ScriptTextEditor : public CodeEditorBase {
 	bool script_is_valid = false;
 
 	RichTextLabel *errors_panel = nullptr;
+	Label *drag_info_label = nullptr;
 
 	Vector<String> functions;
 	List<ScriptLanguage::Warning> warnings;
@@ -173,6 +174,7 @@ protected:
 	void _show_warnings_panel(bool p_show);
 	void _error_clicked(const Variant &p_line);
 	virtual bool _warning_clicked(const Variant &p_line) override;
+	void _on_mouse_exited();
 
 	bool _is_valid_color_info(const Dictionary &p_info);
 	Array _inline_object_parse(const String &p_text);
@@ -197,6 +199,8 @@ protected:
 	Variant get_drag_data_fw(const Point2 &p_point, Control *p_from);
 	bool can_drop_data_fw(const Point2 &p_point, const Variant &p_data, Control *p_from) const;
 	void drop_data_fw(const Point2 &p_point, const Variant &p_data, Control *p_from);
+
+	void _set_drop_info_text(const Dictionary &p_info) const;
 
 	String _get_absolute_path(const String &rel_path);
 