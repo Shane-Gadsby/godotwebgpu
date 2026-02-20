godotengine/godot

Add script editor join line functionality

editor/gui/code_editor.cpp
@@ -909,6 +909,11 @@ void CodeTextEditor::input(const Ref<InputEvent> &event) {
 		accept_event();
 		return;
 	}
+	if (ED_IS_SHORTCUT("script_text_editor/join_line", key_event)) {
+		text_editor->join_lines();
+		accept_event();
+		return;
+	}
 	if (ED_IS_SHORTCUT("script_text_editor/duplicate_selection", key_event)) {
 		text_editor->duplicate_selection();
 		accept_event();

Shane-Gadsby/godotwebgpu

editor/gui/code_editor.cpp
@@ -910,6 +910,11 @@ void CodeTextEditor::input(const Ref<InputEvent> &event) {
 		accept_event();
 		return;
 	}
+	if (ED_IS_SHORTCUT("script_text_editor/join_lines", key_event)) {
+		text_editor->join_lines();
+		accept_event();
+		return;
+	}
 	if (ED_IS_SHORTCUT("script_text_editor/duplicate_selection", key_event)) {
 		text_editor->duplicate_selection();
 		accept_event();