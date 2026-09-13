8aad97148a345b2ead7de67bc6561ea3c241c0e6
Shane Gadsby <schme16@gmail.com>
2026-09-13 21:20:32 +1000

    Verified lots of older implimentations actually worked, and not just trusted it did. squashed a bug related to the way Unorm/Snorm is inteerpreted

--- editor/export/shader_baker_export_plugin.cpp
+++ editor/export/shader_baker_export_plugin.cpp
@@ -425,7 +425,7 @@ void ShaderBakerExportPlugin::_customize_shader_version(ShaderRD *p_shader, RID 
 void ShaderBakerExportPlugin::_process_work_item(WorkItem p_work_item) {
 	if (!tasks_cancelled) {
 		// Only process the item if the tasks haven't been cancelled by the user yet.
-		Vector<RD::ShaderStageSPIRVData> spirv_data = ShaderRD::compile_stages(p_work_item.stage_sources, p_work_item.dynamic_buffers);
+		Vector<RD::ShaderStageSPIRVData> spirv_data = ShaderRD::compile_stages(p_work_item.stage_sources, p_work_item.dynamic_buffers, p_work_item.shader_name);
 		if (unlikely(spirv_data.is_empty())) {
 			ERR_PRINT("Unable to retrieve SPIR-V data for shader.");
 		} else {
