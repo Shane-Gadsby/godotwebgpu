2aace09be789b8901a6492576576b10f0da7e058
Shane Gadsby <schme16@gmail.com>
2026-09-21 15:42:05 +1000

    Add Box3D Physics to the build profile options

--- editor/settings/editor_build_profile.cpp
+++ editor/settings/editor_build_profile.cpp
@@ -74,6 +74,7 @@ const char *EditorBuildProfile::build_option_identifiers[BUILD_OPTION_MAX] = {
 	"disable_physics_3d",
 	"module_godot_physics_3d_enabled",
 	"module_jolt_physics_enabled",
+	"module_box3d_physics_enabled",
 	"module_text_server_fb_enabled",
 	"module_text_server_adv_enabled",
 	"module_freetype_enabled",
@@ -107,6 +108,7 @@ const bool EditorBuildProfile::build_option_disabled_by_default[BUILD_OPTION_MAX
 	false, // PHYSICS_3D
 	false, // PHYSICS_GODOT_3D
 	false, // PHYSICS_JOLT
+	false, // PHYSICS_BOX3D
 	true, // TEXT_SERVER_FALLBACK
 	false, // TEXT_SERVER_ADVANCED
 	false, // DYNAMIC_FONTS
@@ -140,6 +142,7 @@ const bool EditorBuildProfile::build_option_disable_values[BUILD_OPTION_MAX] = {
 	true, // PHYSICS_3D
 	false, // PHYSICS_GODOT_3D
 	false, // PHYSICS_JOLT
+	false, // PHYSICS_BOX3D
 	false, // TEXT_SERVER_FALLBACK
 	false, // TEXT_SERVER_ADVANCED
 	false, // DYNAMIC_FONTS
@@ -173,6 +176,7 @@ const bool EditorBuildProfile::build_option_explicit_use[BUILD_OPTION_MAX] = {
 	false, // PHYSICS_3D
 	false, // PHYSICS_GODOT_3D
 	false, // PHYSICS_JOLT
+	false, // PHYSICS_BOX3D
 	false, // TEXT_SERVER_FALLBACK
 	false, // TEXT_SERVER_ADVANCED
 	false, // DYNAMIC_FONTS
@@ -205,6 +209,7 @@ const EditorBuildProfile::BuildOptionCategory EditorBuildProfile::build_option_c
 	BUILD_OPTION_CATEGORY_PHYSICS, // PHYSICS_3D
 	BUILD_OPTION_CATEGORY_PHYSICS, // PHYSICS_GODOT_3D
 	BUILD_OPTION_CATEGORY_PHYSICS, // PHYSICS_JOLT
+	BUILD_OPTION_CATEGORY_PHYSICS, // PHYSICS_BOX3D
 	BUILD_OPTION_CATEGORY_TEXT_SERVER, // TEXT_SERVER_FALLBACK
 	BUILD_OPTION_CATEGORY_TEXT_SERVER, // TEXT_SERVER_ADVANCED
 	BUILD_OPTION_CATEGORY_TEXT_SERVER, // DYNAMIC_FONTS
@@ -247,6 +252,9 @@ const HashMap<EditorBuildProfile::BuildOption, LocalVector<EditorBuildProfile::B
 			BUILD_OPTION_PHYSICS_3D,
 	} },
 	{ BUILD_OPTION_PHYSICS_JOLT, {
+			BUILD_OPTION_PHYSICS_3D,
+	} },
+	{ BUILD_OPTION_PHYSICS_BOX3D, {
 			BUILD_OPTION_PHYSICS_3D,
 	} },
 	{ BUILD_OPTION_DYNAMIC_FONTS, {
@@ -430,6 +438,7 @@ String EditorBuildProfile::get_build_option_name(BuildOption p_build_option) {
 		TTRC("Physics Server (3D)"),
 		TTRC("Godot Physics (3D)"),
 		TTRC("Jolt Physics"),
+		TTRC("Box3D Physics"),
 		TTRC("Text Server: Fallback"),
 		TTRC("Text Server: Advanced"),
 		TTRC("TTF, OTF, Type 1, WOFF1 Fonts"),
@@ -639,6 +648,7 @@ void EditorBuildProfile::_bind_methods() {
 	BIND_ENUM_CONSTANT(BUILD_OPTION_PHYSICS_3D);
 	BIND_ENUM_CONSTANT(BUILD_OPTION_PHYSICS_GODOT_3D);
 	BIND_ENUM_CONSTANT(BUILD_OPTION_PHYSICS_JOLT);
+	BIND_ENUM_CONSTANT(BUILD_OPTION_PHYSICS_BOX3D);
 	BIND_ENUM_CONSTANT(BUILD_OPTION_TEXT_SERVER_FALLBACK);
 	BIND_ENUM_CONSTANT(BUILD_OPTION_TEXT_SERVER_ADVANCED);
 	BIND_ENUM_CONSTANT(BUILD_OPTION_DYNAMIC_FONTS);
@@ -725,6 +735,11 @@ EditorBuildProfile::EditorBuildProfile() {
 		{ "physics/3d/physics_engine", { "Jolt Physics" } },
 	};
 	build_option_settings.insert(BUILD_OPTION_PHYSICS_JOLT, settings_jolt);
+
+	HashMap<String, LocalVector<Variant>> settings_box3d = {
+		{ "physics/3d/physics_engine", { "DEFAULT", "Box3D Physics" } },
+	};
+	build_option_settings.insert(BUILD_OPTION_PHYSICS_BOX3D, settings_box3d);
 
 	HashMap<String, LocalVector<Variant>> settings_msdfgen = {
 		{ "gui/theme/default_font_multichannel_signed_distance_field", { true } },
